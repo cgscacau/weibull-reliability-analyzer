82e3f812e194a54d6b1c4db5e423b18a1935a7c5
cgscacau <129020973+cgscacau@users.noreply.github.com>
2025-10-25 15:01:11 -0300

    Update __init__.py

--- modules/data_handler/__init__.py
+++ modules/data_handler/__init__.py
@@ -2,5 +2,7 @@
 Módulo de manipulação de dados
 """
 from .file_uploader import FileUploader
+from .data_validator import DataValidator
+from .data_processor import DataProcessor
 
-__all__ = ["FileUploader"]
+__all__ = ["FileUploader", "DataValidator", "DataProcessor"]
