3d74141a7a189c46eb1d16ce734184dc8511a051
cgscacau <129020973+cgscacau@users.noreply.github.com>
2025-10-25 15:13:37 -0300

    Update __init__.py

--- modules/analysis/__init__.py
+++ modules/analysis/__init__.py
@@ -1,1 +1,9 @@
+"""
+Módulo de análise estatística
+"""
+from .weibull_analysis import WeibullAnalysis
+from .reliability_metrics import ReliabilityMetrics
+from .statistical_tests import StatisticalTests
+
+__all__ = ["WeibullAnalysis", "ReliabilityMetrics", "StatisticalTests"]
 
